c7d66ac80681a0d81de1e70d0cd7b32b7a06b414
sava-nj <sava.njegovan@htecgroup.com>
2025-11-27 17:49:59 +0100

    Modify nanoMIPS load/store multiple optimization pass

--- llvm/lib/Target/Mips/NanoMipsLoadStoreMultiple.cpp
+++ llvm/lib/Target/Mips/NanoMipsLoadStoreMultiple.cpp
@@ -32,19 +32,7 @@ static cl::opt<bool> DisableNMLoadStoreMultiple(
 
 namespace {
 struct NMLoadStoreMultipleOpt : public MachineFunctionPass {
-  struct LSIns {
-    unsigned Rt;
-    unsigned Rs;
-    int64_t Offset;
-
-    LSIns(MachineInstr *MI) {
-      Rt = MI->getOperand(0).getReg().id();
-      Rs = MI->getOperand(1).getReg().id();
-      Offset = MI->getOperand(2).getImm();
-    }
-  };
-  using InstrList = SmallVector<MachineInstr *, 11>;
-  using MBBIter = MachineBasicBlock::iterator;
+  using MBBIter = MachineBasicBlock::iterator;   
   static char ID;
   const MipsSubtarget *STI;
   const TargetInstrInfo *TII;
@@ -62,8 +50,10 @@ struct NMLoadStoreMultipleOpt : public MachineFunctionPass {
   bool runOnMachineFunction(MachineFunction &Fn) override;
   unsigned getRegNo(unsigned Reg);
   bool isValidLoadStore(MachineInstr &MI, bool IsLoad);
-  bool isValidNextLoadStore(LSIns Prev, LSIns Next, bool &IsAscending);
   bool generateLoadStoreMultiple(MachineBasicBlock &MBB, bool IsLoad, unsigned MaxSeq);
+  void sortCandidatesBasedOnOffset(SmallVector<MachineInstr *> &LoadStoreList);
+  void saveValidSequence(int &beginIndex, int &endIndex, const SmallVector<MachineInstr *> &Candidate, 
+    SmallVector<SmallVector<MachineInstr *>> &LoadStoreMultipleCandidates);
 };
 } // namespace
 
@@ -78,8 +68,8 @@ bool NMLoadStoreMultipleOpt::runOnMachineFunction(MachineFunction &Fn) {
   for (MachineFunction::iterator MFI = Fn.begin(), E = Fn.end(); MFI != E;
        ++MFI) {
     MachineBasicBlock &MBB = *MFI;
-    Modified |= generateLoadStoreMultiple(MBB, /*IsLoad=*/false, Fn.getFunction().hasOptSize() ? 8 : 2);
-    Modified |= generateLoadStoreMultiple(MBB, /*IsLoad=*/true, Fn.getFunction().hasOptSize() ? 8 : 2);
+    Modified |= generateLoadStoreMultiple(MBB, /*IsLoad=*/false, /*MaxSeq=*/8);
+    Modified |= generateLoadStoreMultiple(MBB, /*IsLoad=*/true, /*MaxSeq=*/8);
   }
 
   return Modified;
@@ -107,8 +97,7 @@ bool NMLoadStoreMultipleOpt::isValidLoadStore(MachineInstr &MI, bool IsLoad) {
   if (IsLoad) {
     // TODO: Handle unaligned loads and stores.
     if (Opcode == Mips::LW_NM || Opcode == Mips::LWs9_NM) {
-      // TODO: Rt and Rs can be equal, but only if that is the last load of
-      // the sequence.
+      // TODO: Rt and Rs can be equal, but only if that is the last load of the sequence.
       Register Rt = MI.getOperand(0).getReg();
       Register Rs = MI.getOperand(1).getReg();
       if (Rt != Rs)
@@ -121,53 +110,33 @@ bool NMLoadStoreMultipleOpt::isValidLoadStore(MachineInstr &MI, bool IsLoad) {
   return false;
 }
 
-bool NMLoadStoreMultipleOpt::isValidNextLoadStore(LSIns Prev, LSIns Next,
-                                                  bool &IsAscending) {
-  if (Prev.Rs != Next.Rs)
-    return false;
-
-  unsigned PrevRtNo = getRegNo(Prev.Rt);
-  if (Next.Offset == Prev.Offset + 4) {
-    // Zero register stores are a special case that does not require
-    // consequent $rt registers, but instead requires all $rt
-    // registers to be $zero.
-    // After processing $31, sequence continues from $16.
-    unsigned DesiredRtNo =
-        PrevRtNo != 0 ? (PrevRtNo == 31 ? 16 : PrevRtNo + 1) : 0;
-    if (Next.Rt != RC.getRegister(DesiredRtNo))
-      return false;
-
-    IsAscending = true;
-    return true;
-  } else if (Next.Offset == Prev.Offset - 4) {
-    // In case the previous register was $16 and the sequence happens to
-    // to go backwards, the next register can be either $15 or $31.
-    if (PrevRtNo == 16) {
-      if (Next.Rt != RC.getRegister(PrevRtNo - 1) &&
-          Next.Rt != RC.getRegister(31))
-        return false;
-    } else {
-      // Zero register stores are a special case that does not require
-      // consequent $rt registers, but instead requires all $rt
-      // registers to be $zero.
-      unsigned DesiredRtNo = PrevRtNo != 0 ? PrevRtNo - 1 : 0;
-      if (Next.Rt != RC.getRegister(DesiredRtNo))
-        return false;
-    }
-    IsAscending = false;
-    return true;
-  }
-  return false;
+void NMLoadStoreMultipleOpt::sortCandidatesBasedOnOffset(SmallVector<MachineInstr *> &LoadStoreList) {
+  auto CompareInstructions = [this](MachineInstr *First, MachineInstr *Second) {
+    // Compare first with second MI offset
+    return First->getOperand(2).getImm() < Second->getOperand(2).getImm();
+  };
+  std::sort(LoadStoreList.begin(), LoadStoreList.end(), CompareInstructions);
 }
 
-bool NMLoadStoreMultipleOpt::generateLoadStoreMultiple(MachineBasicBlock &MBB,
-                                                       bool IsLoad, unsigned MaxSeq) {
-  using LoadStoreSequence = SmallVector<SmallVector<MachineInstr *>>;     
-  LoadStoreSequence Candidates;           // Vector of potential SW/LW consecutive sequences
-  SmallVector<MachineInstr *> Sequence;   // Sequence of consecutive SW/LW instructions
-  bool Modified = false;
+void NMLoadStoreMultipleOpt::saveValidSequence(int &beginIndex, int &endIndex, const SmallVector<MachineInstr *> &Candidate, 
+  SmallVector<SmallVector<MachineInstr *>> &LoadStoreMultipleCandidates) {
+  SmallVector<MachineInstr *> SubSequence;
+  for (auto i = beginIndex; i <= endIndex; ++i) {
+    SubSequence.push_back(Candidate[i]);
+  }
+  LoadStoreMultipleCandidates.push_back(std::move(SubSequence));
+  beginIndex = -1;
+  endIndex = -1;
+}
 
-  // Iterate through machine instructions in machine basic block and add consecutive SW/LW to Candidates vector
+
+bool NMLoadStoreMultipleOpt::generateLoadStoreMultiple(MachineBasicBlock &MBB,
+                                                       bool IsLoad, unsigned MaxSeq) { 
+  SmallVector<SmallVector<MachineInstr *>> Candidates;    // Vector of potential LW/SW consecutive sequences
+  SmallVector<MachineInstr *> Sequence;                   // Sequence of consecutive LW/SW instructions
+  bool Modified = false;
+  // Iterate through machine instructions in the machine basic block and 
+  // collect consecutive LW/SW with same source register into Candidates vector.
   for (auto &MI : MBB) {
     // CFI and debug instructions don't break the sequence.
     if (MI.isCFIInstruction() || MI.isDebugInstr())
@@ -175,136 +144,121 @@ bool NMLoadStoreMultipleOpt::generateLoadStoreMultiple(MachineBasicBlock &MBB,
 
     if (isValidLoadStore(MI, IsLoad)) {
       if (!Sequence.empty()) {
-        // If sequence is not empty check if source registers are same for consecutive SW/LW instructions
+        // If the sequence is not empty, check whether the source registers of the current and previous LW/SW instructions match.
         auto PrevMI = Sequence.back();
-        if (MI.getOperand(1).getReg().id() != PrevMI->getOperand(1).getReg().id()){
+        if (MI.getOperand(1).getReg().id() != PrevMI->getOperand(1).getReg().id()) {
           if (Sequence.size() > 1) {
+            // If source registers differ and the current sequence has at least two MI, save it as a candidate
             Candidates.push_back(Sequence);
           }
           Sequence.clear();
         }
       }
+      // Push MI into the sequence if the sequence is empty or if the
+      // source register matches the previous instruction's source register.
       Sequence.push_back(&MI);
     }  
     else {
       if (Sequence.size() > 1) {
+        // On any non-LW/SW instruction, save sequence if it contains at least two instructions.
         Candidates.push_back(Sequence);
-        Sequence.clear();
+      }
+      Sequence.clear();
+    }
+  }
+
+  // Make sure that the last sequence has been added to the Candidates list.
+  if (Sequence.size() > 1)
+    Candidates.push_back(Sequence);
+
+  // Sequence to change with LWM/SWM
+  SmallVector<SmallVector<MachineInstr *>> LoadStoreMultipleCandidates;
+
+  // Candidates consist of potential optimizable LW/SW consecutive instructions. 
+  for (auto &Candidate : Candidates) {
+    // Sort machine instructions in each candidate vector based on offset.
+    sortCandidatesBasedOnOffset(Candidate);
+
+    /* Print sorted sequences: */
+    // errs() << "\n--- SEQ --- \n";
+    // for (auto &MI : Candidate) {
+    //   MI->dump();
+    // }
+    // errs() << "\n";
+
+    auto beginIdx = -1, endIdx = -1;
+    for (size_t idx = 0; idx < Candidate.size(); idx++) {
+      MachineInstr *Current = Candidate[idx];
+      int64_t CurrentOffset = Current->getOperand(2).getImm();
+      unsigned CurrentRtNo = getRegNo(Current->getOperand(0).getReg().id());
+      
+      if (!isInt<9>(CurrentOffset))
+        continue;
+
+      MachineInstr *Next = (idx + 1 < Candidate.size()) ? Candidate[idx + 1] : nullptr;
+      if (Next) {
+        // Check if there are two valid, consecutive lw/sw instructions.
+        int64_t NextOffset = Next->getOperand(2).getImm();
+        unsigned NextDesiredRtNo = CurrentRtNo != 0 ? (CurrentRtNo == 31 ? 16 : CurrentRtNo + 1) : 0;
+        if ((CurrentOffset == NextOffset - 4) && (NextDesiredRtNo == getRegNo(Next->getOperand(0).getReg().id()))) {
+          if(beginIdx == -1) 
+            beginIdx = idx;
+          endIdx = idx + 1;
+          
+          // The maximum sequence size is 8 (example: indices from 0 to 7)
+          if (endIdx - beginIdx == 7) {
+            ++idx;  // The current MI will be included in the sequence, so skip it in the next iteration.
+            saveValidSequence(beginIdx, endIdx, Candidate, LoadStoreMultipleCandidates); 
+          }
+          
+        }
+        else {
+          // If a valid lw/sw sequence was detected, save it. And start searching for a new one.
+          if (beginIdx != -1) 
+            saveValidSequence(beginIdx, endIdx, Candidate, LoadStoreMultipleCandidates); 
+        }
+      }
+      else {
+        // If this is the last MI in sequence and a valid lw/sw sequence was detected, save it.
+        if (beginIdx != -1) 
+          saveValidSequence(beginIdx, endIdx, Candidate, LoadStoreMultipleCandidates);
       }
     }
   }
 
-  // Candidates consist of potential SW/LW consecutive instructions. 
-  // TODO: Check max.size, check target registers, check offset for sequences.
-  for (auto &Seq : Candidates) {
-    errs() << " --- SEQ --- \n";
-    for (auto &MI : Seq) {
-      MI->dump();
-    }
-    errs() << "\n";
-  }
+  for (auto &Seq : LoadStoreMultipleCandidates) {
+    assert(Seq.size() > 1 && Seq.size() < 9);
 
+    auto *Base = Seq.front();
+    int64_t Offset = Base->getOperand(2).getImm();
+    // Sequence cannot be merged, if the offset is out of range.
+    if (!isInt<9>(Offset))
+      continue;
+
+    auto InsertBefore = std::next(MBBIter(Base));
+    unsigned Opcode = IsLoad ? Mips::LWM_NM : Mips::SWM_NM;
+    auto BMI =
+        BuildMI(MBB, InsertBefore, Base->getDebugLoc(), TII->get(Opcode))
+            .addReg(Base->getOperand(0).getReg(), IsLoad ? RegState::Define : 0)
+            .addReg(Base->getOperand(1).getReg())
+            .addImm(Offset)
+            .addImm(Seq.size());
+    BMI.cloneMergedMemRefs(Seq);
+    for (auto *MI : Seq) {
+      if (MI != Base)
+        BMI.addReg(MI->getOperand(0).getReg(),
+                   IsLoad ? RegState::ImplicitDefine : RegState::Implicit);
+      MBB.erase(MI);
+    }
+
+    /* Print new SWM/LWM instruction: */
+    // errs() << "\n--- SWM/LWM --- \n";
+    // BMI.getInstr()->dump();
+
+    Modified = true;
+  }
   return Modified;
 }
-
-
-//   struct Candidate {
-//     SmallVector<MachineInstr *> Sequence;
-//     bool IsAscending;
-//   };
-//   bool Modified = false;
-//   SmallVector<Candidate> Candidates;
-//   SmallVector<MachineInstr *> Sequence;
-//   bool IsAscending;
-//   for (auto &MI : MBB) {
-//     // CFI and debug instructions don't break the sequence.
-//     if (MI.isCFIInstruction() || MI.isDebugInstr())
-//       continue;
-
-//     if (isValidLoadStore(MI, IsLoad)) {
-//       // Sequences cannot be longer than 8 instructions.
-//       if (Sequence.size() == MaxSeq) {
-//         Candidates.push_back({Sequence, IsAscending});
-//         Sequence.clear();
-//       }
-//       // When starting a new sequence, there's no need to do any checks.
-//       if (Sequence.empty()) {
-//         Sequence.push_back(&MI);
-//         continue;
-//       }
-//       bool ShouldStartNewSequence = false;
-//       bool IsNextAscending;
-//       if (isValidNextLoadStore(Sequence.back(), &MI, IsNextAscending)) {
-//         if (Sequence.size() > 1) {
-//           // In case the next instruction is going in the opposite direction
-//           // from the sequence, start a new sequence.
-//           if (IsAscending != IsNextAscending) {
-//             ShouldStartNewSequence = true;
-//           }
-//         } else {
-//           IsAscending = IsNextAscending;
-//         }
-//       } else {
-//         // In case the next instruction is not a valid successor, save the
-//         // current sequence (if we have one) and create a new sequence.
-//         ShouldStartNewSequence = true;
-//       }
-
-//       if (ShouldStartNewSequence) {
-//         if (Sequence.size() > 1)
-//           Candidates.push_back({Sequence, IsAscending});
-//         Sequence.clear();
-//       }
-
-//       Sequence.push_back(&MI);
-//       continue;
-//     }
-
-//     // At least 2 instructions are neccessary for a valid sequence.
-//     if (Sequence.size() > 1)
-//       Candidates.push_back({Sequence, IsAscending});
-
-//     // Sequence has either ended or has never been started.
-//     if (!Sequence.empty())
-//       Sequence.clear();
-//   }
-
-//   // Make sure that the last sequence has been added to the Candidates list.
-//   if (Sequence.size() > 1)
-//     Candidates.push_back({Sequence, IsAscending});
-
-//   // Separate sequence to avoid removing instructions from MBB while iterating.
-//   for (auto &C : Candidates) {
-//     auto &Seq = C.Sequence;
-
-//     assert(Seq.size() > 1 && Seq.size() < 9);
-
-//     auto *Base = C.IsAscending ? Seq.front() : Seq.back();
-//     int64_t Offset = Base->getOperand(2).getImm();
-//     // Sequence cannot be merged, if the offset is out of range.
-//     if (!isInt<9>(Offset))
-//       continue;
-
-//     auto InsertBefore = std::next(MBBIter(Base));
-//     unsigned Opcode = IsLoad ? Mips::LWM_NM : Mips::SWM_NM;
-//     auto BMI =
-//         BuildMI(MBB, InsertBefore, Base->getDebugLoc(), TII->get(Opcode))
-//             .addReg(Base->getOperand(0).getReg(), IsLoad ? RegState::Define : 0)
-//             .addReg(Base->getOperand(1).getReg())
-//             .addImm(Offset)
-//             .addImm(Seq.size());
-//     BMI.cloneMergedMemRefs(Seq);
-//     for (auto *MI : Seq) {
-//       if (MI != Base)
-//         BMI.addReg(MI->getOperand(0).getReg(),
-//                    IsLoad ? RegState::ImplicitDefine : RegState::Implicit);
-//       MBB.erase(MI);
-//     }
-
-//     Modified = true;
-//   }
-//   return Modified;
-// }
 
 INITIALIZE_PASS(NMLoadStoreMultipleOpt, DEBUG_TYPE, NM_LOAD_STORE_OPT_NAME,
                 false, false)
